d72cb4aae470fabb9099e16fa2bfcd9256c9fb62
Volodymyr Gromoglasov <volodymyrgromoglasov@gmail.com>
2020-12-17 17:39:20 +0000

    improved error handling

--- aflr_client/tts_client.py
+++ aflr_client/tts_client.py
@@ -11,15 +11,20 @@ class AFLRClient:
         self.latest_url = None
 
     async def get_speech_output(self, ssml, voice):
-         async with websockets.connect(
-            "wss://tts.aflr.io/", extra_headers=[("x-api-key", self.api_key)]
-        ) as websocket:
-            await websocket.send('{"text": "' + ssml + '", "voice": "'+ voice +'"}')
-            greeting = await websocket.recv()
-            url = await websocket.recv()
-            self.latest_url = url
-            return   
-            
+        try:
+            async with websockets.connect(
+                "wss://tts.aflr.io/", extra_headers=[("x-api-key", self.api_key)]
+            ) as websocket:
+                await websocket.send(
+                    '{"text": "' + ssml + '", "voice": "' + voice + '"}'
+                )
+                greeting = await websocket.recv()
+                url = await websocket.recv()
+                self.latest_url = url
+                return
+        except:
+            return "Invalid API Key"
+
     def synthesize_speech(self, ssml, voice):
         self.loop.run_until_complete(self.get_speech_output(ssml, voice))
-        return self.latest_url
+        return self.latest_url if self.latest_url != None else "Invalid API Key"
